26d615b0cb1cb8227c3f5dd76a97fc6294e9fdb1
sujal chuwan <12220076.gcit@rub.edu.bt>
2025-09-29 04:35:03 +0600

    homepage

---
+++ myapp/src/Pages/Home1.jsx
@@ -0,0 +1,25 @@
+import Action from "../Components/CallDoAction/Action"
+import Facilities from "../Components/Facilities/Facilities";
+import HeroSection from "../Components/HeroSection/HeroSection";
+import HotelAndFacilities from "../Components/HotelAndFacilities/HotelAndFacilities";
+import HotelAndResort from "../Components/HotelAndResort/HotelAndResort";
+import Offers from "../Components/Offers/Offers";
+import Rooms from "../Components/Rooms/Rooms";
+import Testimonial from "../Components/Testimonial/Testimonial";
+
+const Home1 = () => {
+  return (
+    <>
+      <HeroSection />
+      <Rooms />
+      <HotelAndResort />
+      <HotelAndFacilities />
+      <Action />
+      <Facilities />
+      <Offers />
+      <Testimonial />
+    </>
+  );
+};
+
+export default Home1;
---
+++ myapp/src/Routes/Router.jsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Main/Main.jsx";
+import Home1 from "../Pages/Home1.jsx";
+import ErrorPage from "../Shared/ErrorPage/ErrorPage.jsx";
+import Rooms from "../Components/Rooms/Rooms.jsx";
+import Offers from "../Components/Offers/Offers.jsx";
+import Facilities from "../Components/Facilities/Facilities.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Home1 /> },       // ✅ homepage
+      { path: "rooms", element: <Rooms /> },     // ✅ /rooms
+      { path: "offers", element: <Offers /> },   // ✅ /offers
+      { path: "facilities", element: <Facilities /> }, // ✅ /facilities
+    ],
+  },
+]);
+
+export default router;
